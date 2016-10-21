7de83707c394eee190f75c6bf14705e4cd472f9c
Tam <tamborinehands@gmail.com>
2016-10-21 16:58:38 +0100

    rm

--- anotherjoin.js
+++ anotherjoin.js
@@ -1,5 +1,5 @@
 (function(document) {
   var groups = document.getElementsByClassName(".groupCard tileGrid-tile loading ");
   alert(groups)
-  if (groupContents >= 0) { alert(groupContents[0]) ; alert(groupContents[1])};
+  if (groups.length >= 0) { alert(groupContents[0]) ; alert(groupContents[1])};
 })(document);
