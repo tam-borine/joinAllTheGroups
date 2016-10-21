be92582b04936e84397f8228a655d3aa6289a70c
Tam <tamborinehands@gmail.com>
2016-10-21 17:00:01 +0100

    rs

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 (function(document) {
   var groups = document.getElementsByClassName(".groupCard tileGrid-tile loading ");
-  alert(groups)
+  alert("i work");
+  console.log(groups)
   if (groups.length >= 0) { alert(groupContents[0]) ; alert(groupContents[1])};
 })(document);
