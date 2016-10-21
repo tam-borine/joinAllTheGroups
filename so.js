58a8bdf5eba4a78d884dc9dfeb36582c2971f12b
Tam <tamborinehands@gmail.com>
2016-10-21 17:19:27 +0100

    f

--- so.js
+++ so.js
@@ -1,8 +1,8 @@
-(function(document) {
-  var groups = document.getElementsByClassName(".groupCard tileGrid-tile loading ");
+(function(wind) {
+  var groups = wind.document.getElementsByClassName(".groupCard tileGrid-tile loading ");
   alert("i work");
   for (var i = 0; i < groups.length; i++) {
       console.log(groups[i]);
     }
   console.log(groups)
-})(document);
+})(this);
