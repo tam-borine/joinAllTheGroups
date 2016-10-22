3dc62131219f2122425e71df2025dfa90bcdc07f
Tam <tamborinehands@gmail.com>
2016-10-22 15:49:38 +0100

    fs

--- so.js
+++ so.js
@@ -1,8 +1,8 @@
 (function(window) {
-  var groups = window.document.getElementsByClassName(".groupCard.tileGrid-tile.loading.");
+  var groups = window.document.getElementsByClassName("groupCard.tileGrid-tile.");
   alert("i work");
   for (var i = 0; i < groups.length; i++) {
       console.log(groups[i]);
     }
-  console.log(groups);
+  console.log(groups.innerHTML);
 })(window);
---
+++ store.js
@@ -0,0 +1,13 @@
+var groups = window.document.getElementsByClassName("groupCard tileGrid-tile");
+alert("i work");
+for (var i = 0; i < groups.length; i++) {
+    console.log(groups[i]);
+  }
+console.log(groups);
+
+
+var $groupListEl = $('li.groupCard.tileGrid-tile')
+alert("i work");
+for (var i = 0; i < $groupListEl.length; i++) {
+    console.log($groupListEl[i]);
+  }
