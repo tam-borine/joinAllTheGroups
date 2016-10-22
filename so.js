1ec329e11ec7c0f908130f85a8520f88059b555d
Tam <tamborinehands@gmail.com>
2016-10-22 15:25:31 +0100

    fd

--- so.js
+++ so.js
@@ -1,5 +1,5 @@
 (function(window) {
-  var groups = window.document.getElementsByClassName(".groupCard tileGrid-tile loading ");
+  var groups = window.document.getElementsByClassName(".groupCard.tileGrid-tile.loading.");
   alert("i work");
   for (var i = 0; i < groups.length; i++) {
       console.log(groups[i]);
